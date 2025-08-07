27bb78a7319f8b1d07d28d7982a5c842a5b4be7f
Mark Vrydaghs <sords4@gmail.com>
2025-08-07 15:15:35 -0400

    Add FIND_END_PATHS job and fix typos in function names

--- src/Workers.js
+++ src/Workers.js
@@ -4,13 +4,14 @@ const JOBS = {
     CREATE_SENTENCE: 'create_sentence',
     CHOOSE_RANDOM_PREV_WORD: 'find_rand_prev_word',
     CHOOSE_RANDOM_NEXT_WORD: 'find_rand_next_word',
+    FIND_END_PATHS: 'find_end_paths',
 };
 
 const __END__ = 1;
 const __START__ = 0;
 
 // WIP
-const _generateStentnce = async (corpus, input) => {
+const _generateSentence = async (corpus, input) => {
     const chainWorkers = [
         _createStartChainWorker(corpus, input),
         _createEndChainWorker(corpus, input)
@@ -40,14 +41,21 @@ const _findChainEnd = (corpus, word) => {
     let list = [];
     const pickedWords = new Set();
 
+    const _addToken = (token) => {
+        pickedWords.add(token);
+        list.push(token);
+    };
+
+    // const pathEnd = _pathToEnd(corpus, currentWord);
+    // console.log(pathEnd);
+
     // Keep generating words until we reach the end of the chain
     while (currentWord && chain.has(currentWord) && currentWord !== 1) {
         if (Date.now() - initTime > 6000) {
             const eWords = Array.from(endWords.keys());
             currentWord = eWords[Math.floor(Math.random() * eWords.length)];
             console.warn(`Markov took too long("${sentence}"). Forcing: " ${currentWord}"`);
-            pickedWords.add(currentWord);
-            list.push(currentWord);
+            _addToken(currentWord);
             sentence = ` ${currentWord}`;
             break;
         }
@@ -59,20 +67,87 @@ const _findChainEnd = (corpus, word) => {
             break;
         }
 
-        if(pickedWords.size > 0 && sentence.split(' ').length > (pickedWords.size * 2)) {
+        if(pickedWords.size > 0 && list.length > (pickedWords.size * 2)) {
             break;
         }
 
         // Add the next word to the sentence
         sentence += ' ' + nextWord.split(' ').shift();
 
-        pickedWords.add(nextWord);
-        list.push(nextWord)
+        _addToken(nextWord)
         // Set the current word to the next word
         currentWord = nextWord;
         referenced = { ...referenced, ...chain.get(nextWord)?.refs };
     }
     return {sentence, referenced, list};
+}
+
+// const _findChainEndPaths = (corpus, startingPoint) => {
+//     const { chain, endWords } = corpus;
+//     let outcomes = [];
+//     let referenced = {}; // Do we need this?
+//     let initTime = Date.now();
+//     let currentToken = startingPoint;
+//     let sentence = ''; // I don't think we're going to do this here.
+//     let list = []; // Make this a counter probably
+//     const pickedTokens = new Set();
+
+//     while(currentToken && chain.has(currentToken) && currentToken !== __END__) {
+
+//     }
+// }
+
+const _pathToEnd = ( corpus, currentToken, visits = 1, visited = new Set().add(currentToken)) => {
+    const { chain, endWords } = corpus;
+    const nextTokens = chain.get(currentToken).nextWords;
+    // console.log(currentToken, visits, visited )
+    // If we're at the end of the road
+    if(nextTokens.size === 0 || endWords.get(currentToken)) {
+        return [currentToken];
+    }
+
+    //TEMPORARY MEASURES
+    if(visits > 10) {
+        return [currentToken];
+    }
+
+    // If we've added the same thing too many times
+    if(visits > visits.size * 2) {
+        return [currentToken];
+    }
+
+    //TEMPORARY MEASURES
+    if(endWords.get(currentToken)) {
+        console.warn(currentToken, 'IS AN END WORD AND WE DID NOT END');
+    }
+
+    const paths = [...nextTokens].map(([word, totalValue]) => { return { word , totalValue}});
+
+    const cookieTrail = [];
+    console.log('STARTS: ', paths);
+    paths.forEach((path, p) => {
+        cookieTrail.push([path.word])
+        let forks = [chain.get(path.word).nextWords];
+        let gaz = new Set();
+        for(i = 0; i < forks.length && forks.length < 100 ; i++) {
+            // console.log(forks);
+            forks[i].forEach((value, word) => {
+                gaz.add(word);
+                const nw = chain.get(word).nextWords
+                if(nw.size) {
+                    forks.push(chain.get(word).nextWords)
+                }
+            })
+        }
+        cookieTrail[p].push(forks.map(f=>[...f.keys()]));
+    });
+
+    // const paths = [...nextTokens.keys()].map((nextT) => {
+    //     return { [currentToken]: _pathToEnd(corpus, nextT, visits+1, visited.add(nextT)) };
+    // });
+    console.log('DONE', cookieTrail);
+    cookieTrail.forEach(t => console.log(t.for));
+    return paths;
 }
 
 // A helper function that chooses a random next word from the list of next words for a given word
@@ -91,7 +166,7 @@ const _chooseRandomNextWord = (corpus, word) => {
 
     // Choose the next word based on it's weight.
     /*
-     * What if calculated weights like this and select for multiple paths at once?
+     * What if calculated weights like this and select for multiple paths at once? No... probably not
      * W -> a || b || c
      * a -> t || u
      * b -> v
@@ -132,14 +207,18 @@ const _findChainStart = (corpus, word) => {
     let list = [];
     const pickedWords = new Set();
 
+    const _addToken = (token) => {
+        pickedWords.add(token);
+        list.push(token);
+    };
+
     while (currentWord && chain.has(currentWord) && currentWord !== 0) {
         // Stop if taking too long
         if (Date.now() - initTime > 6000) {
             const sWords = Array.from(startWords.keys());
             currentWord = sWords[Math.floor(Math.random() * sWords.length)];
             console.warn(`Markov took too long("${sentence}"). Forcing: "${currentWord} "`);
-            pickedWords.add(currentWord);
-            list.push(currentWord)
+            _addToken(currentWord);
             sentence = `${currentWord} `;
             break;
         }
@@ -152,15 +231,14 @@ const _findChainStart = (corpus, word) => {
             break;
         }
 
-        if (pickedWords.size > 0 && sentence.split(' ').length > (pickedWords.size * 2)) {
+        if (pickedWords.size > 0 && list.length > (pickedWords.size * 2)) {
             break;
         }
 
-        // Prenpend to previous word to the sentence
+        // Prepend to previous word to the sentence
         sentence = previousWord.split(' ').pop() + ' ' + sentence;
 
-        pickedWords.add(previousWord);
-        list.push(previousWord)
+        _addToken(previousWord);
         // Set the current word to the previous word
         currentWord = previousWord;
         referenced = { ...referenced, ...chain.get(previousWord)?.refs };
@@ -210,13 +288,16 @@ const _removeOverlap = (tokens) => {
 
 switch (workerData?.job) {
     case JOBS.CREATE_SENTENCE:
-        _generateStentnce(workerData.corpus, workerData.options).then(parentPort.postMessage)
+        _generateSentence(workerData.corpus, workerData.options).then(parentPort.postMessage)
         break;
     case JOBS.CHOOSE_RANDOM_PREV_WORD:
         parentPort.postMessage(_findChainStart(workerData.corpus, workerData.options.word));
         break;
     case JOBS.CHOOSE_RANDOM_NEXT_WORD:
         parentPort.postMessage(_findChainEnd(workerData.corpus, workerData?.options.word));
+        break;
+    case JOBS.FIND_END_PATHS:
+        parentPort.postMessage(_pathToEnd(workerData.corpus, workerData.options.word));
         break;
     default:
         throw (`${workerData?.job} is not a valid job.`)
